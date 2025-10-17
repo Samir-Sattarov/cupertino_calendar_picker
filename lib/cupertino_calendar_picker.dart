2434f57caf7a4aa28630bfd6aa6a4372dc8513f7
Samuel Sattarov <samirsattarov2004@gmail.com>
2025-10-17 13:39:37 +0500

    Refactor export statements in cupertino_calendar_picker.dart to simplify module exports

--- lib/cupertino_calendar_picker.dart
+++ lib/cupertino_calendar_picker.dart
@@ -2,30 +2,4 @@
 /// and time selection seamless and visually consistent with iOS design principles.
 library;
 
-export 'src/src.dart'
-    show
-        showCupertinoCalendarPicker,
-        showCupertinoTimePicker,
-        PickerContainerDecoration,
-        CalendarHeaderDecoration,
-        CalendarMonthPickerDayStyle,
-        CalendarMonthPickerBackgroundCircledDayStyle,
-        CalendarMonthPickerDisabledDayStyle,
-        CalendarMonthPickerDefaultDayStyle,
-        CalendarMonthPickerSelectedDayStyle,
-        CalendarMonthPickerSelectedCurrentDayStyle,
-        CalendarMonthPickerCurrentDayStyle,
-        CalendarMonthPickerDecoration,
-        CalendarFooterDecoration,
-        PickerButtonDecoration,
-        CalendarWeekdayDecoration,
-        PickerBackgroundType,
-        CupertinoCalendarMode,
-        CalendarDismissBehavior,
-        CupertinoCalendarPickerButton,
-        CupertinoTimePickerButton,
-        CupertinoCalendar,
-        CupertinoCalendarType,
-        CancelCupertinoCalendarAction,
-        ConfirmCupertinoCalendarAction,
-        CalendarActionDecoration;
+export 'src/src.dart';
